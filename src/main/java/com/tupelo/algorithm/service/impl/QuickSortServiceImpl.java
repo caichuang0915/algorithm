be62a89d583d8dd6f5ed4e5c4eb95355f1ac51a9
tupelo <417670784@qq.com>
2019-07-08 15:06:20 +0800

    quick sort

--- src/main/java/com/tupelo/algorithm/service/impl/QuickSortServiceImpl.java
+++ src/main/java/com/tupelo/algorithm/service/impl/QuickSortServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tupelo.algorithm.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.tupelo.algorithm.service.SortService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,23 +24,105 @@ public class QuickSortServiceImpl implements SortService {
 
      递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序；
 
-     时间复杂度 O(nlogn)
+     平均时间复杂度是 O（nlogn），最坏情况下的时间复杂度是 O（n^2）。
      */
     @Override
     public void sort(int[] array) {
+        quickSort(array,0,array.length-1);
+    }
 
+    private static int[] quickSort(int[] array, int left, int right){
+        if(left<right){
+            int pari = partition(array,left,right);
+            quickSort(array,left,pari-1);
+            quickSort(array,pari+1,right);
+        }
+        return array;
+    }
 
+    private static int[] quickSortBak(int[] array, int left, int right){
+        if(left<right){
+            int pari = partitionBak(array,left,right);
+            quickSort(array,left,pari-1);
+            quickSort(array,pari+1,right);
+        }
+        return array;
     }
 
 
-    private int partition(int[] arr,int left,int right){
+    private static int partition(int[] arr,int left,int right){
 
-        return 0;
+        int start = left;
+        int pivot = arr[left];
+
+        while(left != right){
+            // 左侧小于选定的值 指针向右移一位
+            while(left<right && arr[left]<=pivot){
+                left++;
+            }
+            // 右侧大于选定的值 指针向右移一位
+            while (right>left && arr[right] > pivot){
+                right--;
+            }
+            // 如果还没有重合 则交换两个指针处的值
+            if(left<right){
+                swap(arr,left,right);
+            }
+        }
+
+        // 指针重合 与 选定位置交换
+        if(arr[left]<pivot){
+            swap(arr,left,start);
+            return left;
+        }else{
+            swap(arr,left-1,start);
+        }
+        return left-1;
     }
 
-    private void swap(int[] arr,int i ,int j){
+    private static void swap(int[] arr,int i ,int j){
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,234,546,78,45,23,88,50,100,32,63,2932};
+        quickSortBak(array,0,array.length-1);
+        System.out.println(JSON.toJSONString(array));
+    }
+
+
+    private static int partitionBak(int[] arr,int left,int right){
+
+        int pivot = arr[left];
+        int start = left;
+
+        while(left<right){
+
+            // 从右边开始比较
+            while (left<right){
+                if(arr[right] < pivot){
+                    arr[left] = arr[right];
+                    start = right;
+                    left++;
+                    break;
+                }
+                right--;
+            }
+            // 从左边开始比较
+            while (left<right){
+                if(arr[left]>pivot){
+                    arr[right] = arr[left];
+                    start = left;
+                    right--;
+                    break;
+                }
+                left++;
+            }
+
+        }
+        arr[start] = pivot;
+        return start;
     }
 }
